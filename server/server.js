d4269d352c67a082b3c70ddb336f9ce288e2950d
tannerlsmith <tanner.smith@uvm.edu>
2021-08-08 21:36:23 -0400

    setup server.js

---
+++ server/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express
+
+app.request(express.json())
+
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = new Map([
+    [1, { priceInCents: 10000, name: 'necktie' }],
+    [2, {priceInCents: 20000, name: 'learn server.js' }],
+])
+
+app.listen(3001)
